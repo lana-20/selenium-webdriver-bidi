4c9b0972637ba272681adcd33417e9ecf90d2bc3
Lana Begunova <70295997+lana-20@users.noreply.github.com>
2023-09-07 09:32:45 -0700

    selenium_cdp_support_console_logs.java

---
+++ selenium_cdp_support_console_logs.java
@@ -0,0 +1,12 @@
+ChromeDriver driver = new ChromeDriver();
+DevTools devTools = driver.getDevTools();
+devTools.createSession();
+devTools.send(Log.enable());
+devTools.addListener(Log.entryAdded(),
+	logEntry -> (
+		System.out.println(“log: ” + logEntry.getText());
+ 		System.out.println(“level: ” + logEntry.getLevel());
+));
+driver.ger(“http://example.com”);
+// Check the terminal output for the browser console messages.
+driver.quit();
