d20c02cfebed258c1e1e47f9e00208f6968b4b90
Lana Begunova <70295997+lana-20@users.noreply.github.com>
2023-09-08 13:23:15 -0700

    code cleanup

--- selenium_cdp_support_geolocation.java
+++ selenium_cdp_support_geolocation.java
@@ -5,10 +5,10 @@ DevTools devTools = driver.getDevTools();
 devTools.createSession();
 
 devTools.send(
-Emulation.setGeolocationOverride(
-Optional.of(47.604653),
-Optional.of(-122.335461),
-Optional.of(1)));
+  Emulation.setGeolocationOverride(
+    Optional.of(47.604653),
+    Optional.of(-122.335461),
+    Optional.of(1)));
 
 driver.get(“https://my-location.org/”);
 driver.quit();
